ad15771fab1d207088243fc4cb885a7d3299f630
김기윤 <crakel528@ajou.ac.kr>
2021-07-19 18:38:40 +0900

    0719 일반형 돌격형 적 생성

--- Quarter_Action/Assets/Scripts/Enemy.cs
+++ Quarter_Action/Assets/Scripts/Enemy.cs
@@ -5,10 +5,14 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    public enum Type { A, B, C };
+    public Type enemyType;
     public int maxHealth;
     public int curHealth;
     public Transform target;
+    public BoxCollider meleeArea;
     public bool isChase;
+    public bool isAttack;
 
     Rigidbody rigid;
     BoxCollider boxCollider;
@@ -31,8 +35,10 @@ public class Enemy : MonoBehaviour
         anim.SetBool("isWalk", true);
     }
     void Update() {
-        if(isChase)
-        nav.SetDestination(target.position);
+        if(nav.enabled) {
+            nav.SetDestination(target.position);
+            nav.isStopped = !isChase;
+        }
     }
     void FreezeVelocity() {
         if (isChase) {
@@ -41,7 +47,82 @@ public class Enemy : MonoBehaviour
         }
     }
 
+    void Targeting() {
+        float targetRadius = 1.5f;
+        float targetRange = 3f;
+
+        switch (enemyType) { 
+            case Type.A:
+                targetRadius = 1.5f;
+                targetRange = 3f;
+                break;
+            case Type.B:
+                targetRadius = 1f;
+                targetRange = 12f;
+                break;
+            case Type.C:
+            
+                break;
+        }
+
+        RaycastHit[] rayHits =
+            Physics.SphereCastAll(transform.position,
+            targetRadius,
+            transform.forward,
+            targetRange,
+            LayerMask.GetMask("Player"));
+        
+        if(rayHits.Length > 0 && !isAttack) {
+            StartCoroutine(Attack());
+        }
+    }
+
+    IEnumerator Attack() {
+        isChase = false;
+        isAttack = true;
+        anim.SetBool("isAttack", true); 
+    
+        switch (enemyType) { 
+            case Type.A:
+                yield return new WaitForSeconds(0.2f);
+                meleeArea.enabled = true;
+
+                yield return new WaitForSeconds(1f);
+                meleeArea.enabled = false;
+
+                yield return new WaitForSeconds(1f);
+                break;
+            case Type.B:
+                yield return new WaitForSeconds(0.1f);
+                rigid.AddForce(transform.forward * 20, ForceMode.Impulse);
+                meleeArea.enabled = true;
+
+                yield return new WaitForSeconds(0.5f);
+                rigid.velocity = Vector3.zero;
+                meleeArea.enabled = false;
+
+                yield return new WaitForSeconds(2f);
+                break;
+            case Type.C:
+            
+                break;
+        }
+
+        yield return new WaitForSeconds(0.2f);
+        meleeArea.enabled = true;
+
+        yield return new WaitForSeconds(1f);
+        meleeArea.enabled = false;
+
+        yield return new WaitForSeconds(1f);
+
+        isChase = true;
+        isAttack = false;
+        anim.SetBool("isAttack", false);
+
+    }
     void FixedUpdate() {
+        Targeting();
         FreezeVelocity();
     }
     void OnTriggerEnter(Collider other) {
